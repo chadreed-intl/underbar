3e0bffd478245a6ac30f8fa7a72ce6ce6add7ebd
Tyson Daugherty <Tyson@TYSONs-MacBook-Pro.local>
2013-03-12 03:30:10 -0700

    a bit of last and first working

--- underscore_broken.js
+++ underscore_broken.js
@@ -58,10 +58,26 @@
   // Return an array of the last n elements of an array. If n is undefined,
   // return just the last element.
   var last = function(array, n) {
+    if (n == undefined) {
+      return array.slice(array.length-1);
+    } else if (n > array.length-1) {
+      return array.slice(0);
+    } else {
+      return array.slice(array.length - n);
+    }
   };
 
   // Like last, but for the first elements
   var first = function(array, n) {
+    if (n == undefined) {
+      return array.slice(0,1);
+    } else if (n < 1) {
+      return array.slice(0,0);
+    } else if (n >= 0) {
+      return array.slice(0, n-1);
+    } else {
+      return array.slice(0);
+    }
   };
 
   // Reduces an array or object to a single value by repetitively calling
